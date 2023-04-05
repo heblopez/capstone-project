a0b9c8a918403d7ef2b44a6a9dd7237764fe7d34
Heberth Lopez <heberth.lopez.19@gmail.com>
2023-04-05 13:37:14 -0500

    Added index, create, show, update, destroy methods in properties controller

---
+++ rails-get-that-home/app/controllers/properties_controller.rb
@@ -0,0 +1,50 @@
+class PropertiesController < ApplicationController
+  skip_before_action :authorize, only: :index
+
+  def index
+      properties = Property.all
+      render json: properties, status: :ok
+  end
+
+  def create
+    property = Property.new(property_params)
+    if property.save
+      render json: property, status: :created
+    else
+      respond_unauthorized("Error! the property could not be created")
+    end
+  end
+
+  def update
+    property = Property.find(params[:id])
+    if property.update(property_params)
+      render json: property, status: :ok
+    else
+      respond_unauthorized("Error! the property could not be updated")
+    end
+  end
+
+  def show
+    property = Property.find(params[:id])
+    if property
+      render json: property, status: :ok
+    else
+      respond_unauthorized("Error! the property could not be found")
+    end
+  end
+
+  def destroy
+    property = Property.find(params[:id])
+    if property.destroy
+      status: :ok
+    else
+      respond_unauthorized("Error! the property could not be deleted")
+    end
+  end
+
+  private
+
+  def property_params
+    params.permit(:addres, :type_operation, :monthly_rent, :maintanance, :price, :type, :bedrooms, :bathrooms, :area, :pets_allowed, :description)
+  end
+end
--- rails-get-that-home/config/routes.rb
+++ rails-get-that-home/config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
     get "/logout" => "sessions#destroy"
 
     resources :users, only: %i[create show update]
+    resources :properties, only: %i[create show update index destroy]
 end
