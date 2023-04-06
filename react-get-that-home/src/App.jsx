991c70dc1a29fb992a16c1f99c782d6385455f7e
Paul Llanque <llanquepol@gmail.com>
2023-04-05 21:33:01 -0500

    add action component

--- react-get-that-home/src/App.jsx
+++ react-get-that-home/src/App.jsx
@@ -1,22 +1,27 @@
 import { useState } from 'react';
 import SignupForm from './component/Inputs/SignupForm';
 import Description from './component/Description/Description';
+import Action from './component/Action/Action';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className='App'>
-      <Description
-        price={'3,000'}
-        type={'Aparment'}
-        address={'86872 Jacob Gateway, Durganport, WV 48044'}
-        bedCount={4}
-        bathroomCount={2}
-        petss={true}
-        classSpanAparment={'apartment'}
-        classSpanPrice={'price'}
-      />
+      <BrowserRouter>
+        <Description
+          price={'3,000'}
+          type={'Aparment'}
+          address={'86872 Jacob Gateway, Durganport, WV 48044'}
+          bedCount={4}
+          bathroomCount={2}
+          petss={true}
+          classSpanAparment={'apartment'}
+          classSpanPrice={'price'}
+        />
+        <Action isActive='active' />
+      </BrowserRouter>
     </div>
   );
 }
