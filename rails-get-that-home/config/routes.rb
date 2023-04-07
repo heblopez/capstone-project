14a4549213ccdfc37b7065954622e02b809bd638
Cristoper Yarleque <caypsaturno@gmail.com>
2023-04-06 23:16:55 -0500

    ADD: favorites and contacted controllers

--- rails-get-that-home/config/routes.rb
+++ rails-get-that-home/config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
     post "/login" => "sessions#create"
     get "/logout" => "sessions#destroy"
 
-    resources :users, only: %i[create show update]
+    resources :users, only: %i[create show update] do
+        resources :favorites, only: %i[index update]
+        resources :contacted, only: %i[index update]
+    end
+
     resources :properties, only: %i[create show update index destroy]
 end
