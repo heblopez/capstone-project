6606902034a1c9f9be7386871816789ac54174dc
Paul Llanque <llanquepol@gmail.com>
2023-04-09 16:12:20 -0500

    add buttons

---
+++ react-get-that-home/src/pages/FilterOptions-UI.js
@@ -0,0 +1,81 @@
+import styled from '@emotion/styled';
+import { colors } from '../styles/colors';
+
+const SectionFind = styled.div`
+  width: 100%;
+  padding: 32px 120px;
+
+  div.container {
+    padding: 0 32px;
+  }
+`;
+
+const BarOption = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  div.input-search {
+    input {
+      border: none;
+      width: 100%;
+      height: 36px;
+      border: 1px solid ${colors.pink};
+      border-radius: 8px;
+      padding: 8px;
+      background-color: transparent;
+      margin: 0;
+
+      :focus {
+        outline: none;
+        border: 1px solid #eb6390;
+      }
+
+      ::placeholder {
+        color: ${colors.lightGray};
+      }
+
+      &.holder {
+        padding-left: 36px;
+      }
+    }
+  }
+
+  div.btns {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+
+    div.price {
+      button {
+        width: 78px;
+      }
+    }
+
+    div.property-type {
+      button {
+        width: 162px;
+      }
+    }
+
+    div.beds-baths {
+      button {
+        width: 147px;
+      }
+    }
+
+    div.more {
+      button {
+        width: 109px;
+      }
+    }
+  }
+
+  div.buy-rent {
+    button {
+      width: 215px;
+    }
+  }
+`;
+
+export { BarOption, SectionFind };
--- react-get-that-home/src/pages/FindPage.jsx
+++ react-get-that-home/src/pages/FindPage.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from 'react';
+import Button from '../components/Button/Button';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+import { SectionFind, BarOption } from './FilterOptions-UI';
 
 const FindPage = () => {
   return (
-    <div>FindPage</div>
-  )
-}
+    <SectionFind>
+      <div className='container'>
+        <BarOption>
+          <div className='input-search'>
+            <input
+              type='text'
+              name='search'
+              id='search'
+              placeholder='Search by address'
+            />
+          </div>
+          <div className='btns'>
+            <div className='price'>
+              <Button>Price</Button>
+            </div>
+            <div className='property-type'>
+              <Button>property type</Button>
+            </div>
+            <div className='beds-baths'>
+              <Button>beds & baths</Button>
+            </div>
+            <div className='more'>
+              <Button>
+                more
+                <MdKeyboardArrowDown />
+              </Button>
+            </div>
+          </div>
+          <div className='buy-rent'>
+            <Button type={'secundary'}>
+              Buying & Renting
+              <MdKeyboardArrowDown />
+            </Button>
+          </div>
+        </BarOption>
 
-export default FindPage
+        <div>
+          <p>24 Properties found</p>
+          <div> Lista of houses</div>
+        </div>
+      </div>
+    </SectionFind>
+  );
+};
+
+export default FindPage;
