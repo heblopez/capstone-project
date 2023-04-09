3e73ffea6cd4ee06fbbf10943c9cde1ba0d51ebd
Paul Llanque <llanquepol@gmail.com>
2023-04-08 23:34:13 -0500

    update session services

--- react-get-that-home/src/services/auth-services.js
+++ react-get-that-home/src/services/auth-services.js
@@ -1,8 +1,8 @@
-
 import { TOKEN_KEY } from '../config';
 import apiFetch from './apiFetch';
 
 export function login(credentials) {
+
   return apiFetch('/login', { body: credentials }).then((user) => {
     const { token, ...userdata } = user;
     sessionStorage.setItem(TOKEN_KEY, token);
