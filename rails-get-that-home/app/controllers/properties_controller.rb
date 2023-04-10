58fabb89480f11c3b7f0a16965f89800b958308e
Cristoper Yarleque <caypsaturno@gmail.com>
2023-04-10 13:45:15 -0500

    CHORE: seed

--- rails-get-that-home/app/controllers/properties_controller.rb
+++ rails-get-that-home/app/controllers/properties_controller.rb
@@ -3,7 +3,7 @@ class PropertiesController < ApplicationController
 
   def index
       properties = Property.all
-      render json: properties, status: :ok
+      render json: properties.as_json(methods: :photo_urls), status: :ok
   end
 
   def create
@@ -34,7 +34,7 @@ class PropertiesController < ApplicationController
   def show
     property = Property.find(params[:id])
     if property
-      render json: property, status: :ok
+      render json: property.as_json(methods: :photo_urls), status: :ok
     else
       respond_unauthorized("Error! the property could not be found")
     end
