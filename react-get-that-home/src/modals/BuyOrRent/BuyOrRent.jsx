eb2de2d100e97de155bcc322704d4f38a1e22838
Paul Llanque <llanquepol@gmail.com>
2023-04-11 12:59:33 -0500

    add buying and renting modal

---
+++ react-get-that-home/src/modals/BuyOrRent/BuyOrRent.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import BuyRent from './BurOrRent-UI';
+
+const BuyOrRent = ({ onHandle, data }) => {
+  return (
+    <BuyRent>
+      <div className='container'>
+        <div className='inputs'>
+          <div className='input'>
+            <input
+              type='checkbox'
+              name='both'
+              id='both'
+              checked={data.both}
+              onChange={onHandle}
+            />
+            <label htmlFor='both'>Both</label>
+          </div>
+
+          <div className='input'>
+            <input
+              type='checkbox'
+              name='buying'
+              id='buying'
+              onChange={onHandle}
+            />
+            <label htmlFor='buying'>Buying</label>
+          </div>
+
+          <div className='input'>
+            <input
+              type='checkbox'
+              name='renting'
+              id='renting'
+              onChange={onHandle}
+            />
+            <label htmlFor='renting'>Renting</label>
+          </div>
+        </div>
+      </div>
+    </BuyRent>
+  );
+};
+
+export default BuyOrRent;
