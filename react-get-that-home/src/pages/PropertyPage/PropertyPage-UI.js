2cb3ad0d6c3b03540e5bf2d964e6074650b24529
Heberth Lopez <heberth.lopez.19@gmail.com>
2023-04-13 18:46:11 -0500

    Added PropertyPage with styles

---
+++ react-get-that-home/src/pages/PropertyPage/PropertyPage-UI.js
@@ -0,0 +1,125 @@
+import styled from "@emotion/styled";
+import { colors } from "../../styles/colors";
+
+const MainSection = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  gap: 16px;
+`;
+
+const SideBar = styled.div`
+  width: 15%;
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  margin: 0;
+  width: 85%;
+  min-width: 600px;
+  padding: 32px 10%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  div.container-photos {
+    width: 512px;
+    height: 384px;
+  }
+
+  div.title-and-price {
+    width: 100%;
+    display: flex;
+    margin: 1rem 0;
+    justify-content: space-between;
+    gap: 0.5rem;
+    align-items: center;
+
+    p.text-xl {
+      font-size: 36px;
+      font-weight: 400;
+      line-height: 48px;
+      letter-spacing: 0.25px;
+      color: ${colors.darkGray};
+    }
+
+    p.city-text {
+      font-size: 16px;
+      font-weight: 400;
+      line-height: 24px;
+      letter-spacing: 0.15px;
+      color: ${colors.gray};
+    }
+
+    div.container-price {
+      display: flex;
+      flex-direction: column;
+
+      div.price-dollar {
+        display: flex;
+        align-items: center;
+        gap: 12px;
+
+        svg.dollar-icon {
+          color: ${colors.grey};
+          size: 40px;
+        }
+      }
+    }
+
+    span.text-maintanance {
+      font-size: 20px;
+      font-weight: 500;
+      line-height: 28px;
+      letter-spacing: 0.15px;
+      color: ${colors.gray};
+      align-self: flex-end;
+    }
+  }
+
+  div.info-property {
+    width: 100%;
+    display: flex;
+    padding: 1rem 0;
+    justify-content: space-between;
+    gap: 1rem;
+    border-width: 1px 0;
+    border-style: solid;
+    border-color: ${colors.darkpink2};
+
+    div.info-detail {
+      display: flex;
+      align-items: center;
+      gap: 0.4em;
+      font-weight: 400;
+      font-size: 24px;
+      line-height: 32px;
+      color: ${colors.gray};
+
+      svg.icon-info {
+        color: ${colors.gray};
+        size: 24px;
+      }
+
+      img.icon-info {
+        width: 24px;
+      }
+    }
+  }
+
+  div.about-location {
+    width: 100%;
+    margin-top: 16px;
+
+    h2 {
+      font-weight: 500;
+      font-size: 22px;
+      line-height: 28px;
+      letter-spacing: 0.15px;
+      color: ${colors.darkpink2};
+      margin: 8px 0;
+  }
+`;
+
+export { MainSection, SideBar, Wrapper };
---
+++ react-get-that-home/src/pages/PropertyPage/PropertyPage.jsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Properties from '../../services/properties-services';
+import { MainSection, Wrapper, SideBar} from './PropertyPage-UI';
+import { RiMoneyDollarCircleLine } from 'react-icons/ri';
+import { BiBed, BiBath, BiArea } from 'react-icons/bi';
+import PetsIcon from '../../assets/pets.svg';
+
+const PropertyPage = () => {
+  const { id } = useParams();
+
+  const [property, setProperty] = useState({});
+
+  useEffect(() => {
+    const property = setTimeout(() => {
+      Properties.getProp(id)
+        .then((prop) => setProperty(prop))
+        .catch(console.log);
+    });
+    return () => clearTimeout(property);
+  }, []);
+
+  const { address, price,monthly_rent, maintanance, bedrooms, bathrooms, area, pets_allowed, description, photo_urls } = property;
+
+  const street = address ? address.split(',')[0].trim() : '';
+  const city = address ? address.split(',')[1].trim() : '';
+  const state = address ? address.split(',')[2].trim().split(' ')[0] : '';
+
+  return (
+    <MainSection>
+      <Wrapper>
+        <div className='container-photos'>
+          <img
+          src={photo_urls ? photo_urls[0] : ''}
+          alt='Property photo'
+          />
+        </div>
+        <div className='title-and-price'>
+          <div>
+          <p className='text-xl'>{street}</p>
+          <p className='city-text'>{city}, {state}</p>
+          </div>
+          <div className='container-price'>
+            <div className='price-dollar'>
+              <RiMoneyDollarCircleLine className='dollar-icon' />
+              <p className='text-xl'> {monthly_rent}</p>
+            </div>
+            <span className='text-maintanance'>+ {maintanance}</span>
+          </div>
+        </div>
+        <div className='info-property'>
+          <div className='info-detail'>
+            <BiBed className='icon-info'/>
+            {bedrooms} bedrooms
+          </div>
+          <div className='info-detail'>
+            <BiBath className='icon-info'/>
+            {bathrooms} bathrooms
+          </div>
+          <div className='info-detail'>
+            <BiArea className='icon-info'/>
+            {area} m2
+          </div>
+          <div className='info-detail'>
+            <img src={PetsIcon} alt='pets-icon' className='icon-info' />
+            {pets_allowed ? 'Pets allowed' : 'No pets allowed'}
+          </div>
+        </div>
+        <div className='about-location'>
+          <h2>About this property</h2>
+          <p>{description}</p>
+        </div>
+        <div className='about-location'>
+          <h2>Location</h2>
+          <p>{address}</p>
+        </div>
+    </Wrapper>
+    <SideBar>Boton para login</SideBar>
+    </MainSection>
+  );
+};
+
+export default PropertyPage;
