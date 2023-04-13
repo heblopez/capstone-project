8e11b66b1c9d11d5814d9bf75ebbd7db66a9a77f
Paul Llanque <llanquepol@gmail.com>
2023-04-13 00:21:33 -0500

    add userProvider

--- react-get-that-home/src/main.jsx
+++ react-get-that-home/src/main.jsx
@@ -4,13 +4,16 @@ import App from './App';
 import { Global } from '@emotion/react';
 import { global, reset } from './styles';
 import { PropertyProvider } from './context/Context';
+import { UserProvider } from './context/UserContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Global styles={reset} />
     <Global styles={global} />
-    <PropertyProvider>
-      <App />
-    </PropertyProvider>
+    <UserProvider>
+      <PropertyProvider>
+        <App />
+      </PropertyProvider>
+    </UserProvider>
   </React.StrictMode>
 );
