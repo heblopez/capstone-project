2aa308982e45031304bc87dcf4ccff8081b4fcd7
Paul Llanque <llanquepol@gmail.com>
2023-04-13 21:08:30 -0500

    add cards in show propetties

--- react-get-that-home/src/pages/PropertyPage.jsx
+++ react-get-that-home/src/pages/PropertyPage.jsx
@@ -1,8 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import Properties from '../services/properties-services';
+import Target from '../components/Target/Target';
+import Button from '../components/Button/Button';
+import { AiOutlineUserAdd, AiOutlineHeart } from 'react-icons/ai';
+import { useUser } from '../context/UserContext';
+import { useShow } from '../context/ShowContext';
 
 const PropertyPage = () => {
+  const { user } = useUser();
+  const { handleShow } = useShow();
+
+  const whoIs = user ? user.role : '';
+
   const { id } = useParams();
 
   const [property, setProperty] = useState({});
@@ -16,15 +26,14 @@ const PropertyPage = () => {
     return () => clearTimeout(property);
   }, []);
 
-  console.log(property);
-
   return (
-    <div>
+    <div style={{ display: 'flex', gap: '15px' }}>
       <div>
         <img
           src={property.photo_urls ? property.photo_urls[0] : ''}
           alt='Photo'
         />
+
         <p>{property.address}</p>
         <div>
           <p>$ {property.price}</p>
@@ -44,6 +53,44 @@ const PropertyPage = () => {
         <div>
           <p>Location:</p>
         </div>
+      </div>
+
+      {/* Esta parte es para los botones */}
+      {/* llamada al login */}
+      <div>
+        {!user && (
+          <Target>
+            <p>Log in or Join to contact the advertiser</p>
+            <div className='btn-login' onClick={handleShow}>
+              <Button>
+                <AiOutlineUserAdd />
+                login
+              </Button>
+            </div>
+          </Target>
+        )}
+
+        {/* cuando el login es home seeker */}
+        {whoIs === 'home_seeker' && (
+          <Target>
+            <div className='btn-contact' onClick={handleShow}>
+              <Button>contact advertiser</Button>
+              <div>
+                <AiOutlineHeart />
+                <p> Add to favorites</p>
+              </div>
+            </div>
+          </Target>
+        )}
+
+        {/* cuando el login es landlord */}
+        {whoIs === 'landlord' && (
+          <div className='btn-edit_property'>
+            <Link to={`/edit/property/${id}`}>
+              <Button>edit property</Button>
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
