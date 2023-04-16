7f6d10a3a4efeb1b232d8236ae16a33fe15e7472
Paul Llanque <llanquepol@gmail.com>
2023-04-15 22:12:50 -0500

    add update method

--- react-get-that-home/src/services/properties-services.js
+++ react-get-that-home/src/services/properties-services.js
@@ -1,5 +1,5 @@
 import apiFetch from './apiFetch';
-import { BASE_URI, TOKEN_KEY } from "../config"
+import { BASE_URI, TOKEN_KEY } from '../config';
 
 const Properties = {
   async get() {
@@ -29,15 +29,33 @@ const createProperty = async (new_property) => {
     await fetch(`${BASE_URI}/properties`, {
       method: 'POST',
       headers: {
-        'Authorization': `Bearer ${token}`
+        Authorization: `Bearer ${token}`,
       },
       body: new_property,
     });
+    location.reload();
   } catch (error) {
     console.error(error);
   }
 };
 
-export {
-  createProperty
-}
+const updateProperty = async (update_property, id) => {
+  const token = sessionStorage.getItem(TOKEN_KEY);
+  console.log('from update');
+  try {
+    const response = await fetch(`${BASE_URI}/properties/${id}`, {
+      method: 'PATCH',
+      headers: {
+        Authorization: `Token token=${token}`,
+      },
+      body: update_property,
+    });
+    const data = response.json();
+    console.log(data);
+    location.reload();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export { createProperty, updateProperty };
