49922697c6a54cc843abf29b47ec26a484cb99f5
Paul Llanque <llanquepol@gmail.com>
2023-04-24 22:24:29 -0500

    update Card component

--- react-get-that-home/src/components/Card/Card.jsx
+++ react-get-that-home/src/components/Card/Card.jsx
@@ -5,9 +5,10 @@ import { CardContainer } from './Card-UI';
 import { Link } from 'react-router-dom';
 
 const Card = ({ property, section, favorite }) => {
+  const { id } = property;
   return (
     <CardContainer>
-      <Link to={`/property/${property.id}`} className='card'>
+      <Link to={`/property/${id}`} className='card'>
         <Property prop={property} favorite={favorite} />
       </Link>
       <Action id={property.id} section={section} />
